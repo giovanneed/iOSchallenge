f142d331eff747bbdd17bd21fbd322cbb041e690
Johannes Berger <johannes@getmimo.com>
2017-02-15 17:21:50 +0100

    Initial Commit

---
+++ MimoiOSCodingChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MimoiOSCodingChallenge
+//
+//  Created by Johannes Berger on 15.02.17.
+//  Copyright © 2017 Mimohello GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MimoiOSCodingChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MimoiOSCodingChallenge
+//
+//  Created by Johannes Berger on 15.02.17.
+//  Copyright © 2017 Mimohello GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
